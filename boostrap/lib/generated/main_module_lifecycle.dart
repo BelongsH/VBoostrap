b8f46992e23e82e9a80ce81a09889d7d3ebdf564
liuhuiliang <liuhuiliang@vv.cn>
2022-12-20 17:08:31 +0800

    init

---
+++ boostrap/lib/generated/main_module_lifecycle.dart
@@ -0,0 +1,11 @@
+import 'package:boostrap_base/src/base_module_init.dart';
+import 'package:boostrap_base/src/module_generator.dart';
+
+class MainModuleGenerator extends ModuleGenerator {
+    @override
+  List<BaseModuleInit> collectModules() {
+    final List<BaseModuleInit> modules=[];
+
+    return modules;
+  }
+}
